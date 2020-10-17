a570e8f707a9833b1ed2c04a97f7b58a3b085536
FitrahMelandri <dwipra17si@mahasiswa.pcr.ac.id>
2020-10-17 16:00:28 +0700

    bootcamp

---
+++ product.php
@@ -0,0 +1,47 @@
+<?php
+
+class product
+    {
+    private $nama;
+    private $stok;
+    private $kategori;
+
+    function __construct($nama="rinso", $stok=10 , $kategori="pencuci pakaian") 
+    {
+        $this->nama = $nama;
+        $this->stok = $stok;
+        $this->kategori = $kategori;
+    }
+    function call_identity()
+    {
+        echo "<p>"."nama : ".$this->nama. " stok : ".
+        $this->stok. " kategori : ". $this->kategori."</p>";
+    }
+    function set_nama($nama)
+    {
+        $this->nama = $nama;
+    }
+    function get_nama()
+    {
+        return $this->nama;
+    }
+    function set_stok($stok)
+    {
+        $this->stok = $stok;
+    }
+    function get_stok()
+    {
+        return $this->stok;
+    }
+    function set_kategori($kategori)
+    {
+        $this->kategori = $kategori;
+    }
+    function get_kategori()
+    {
+        return $this->kategori;
+    }
+}
+
+$prdct = new product("rinso", 10, "pencuci pakaian");
+$prdct->call_identity();
